979b9770496429a7c9d25c316bce1fa0e8aa922a
GabrielMendesCosta <gabrielmendescst@gmail.com>
2023-11-16 10:44:57 -0300

    feat: created the estatic filter

--- src/pages/Menu/Filters/index.tsx
+++ src/pages/Menu/Filters/index.tsx
@@ -1,14 +1,30 @@
+import React from "react";
 import filters from "./filters.json"
 import styles from "./Filters.module.scss"
+import classNames from "classnames";
 
 type IOptions = typeof filters[0];
 
-export default function Filters(){
-    function selectFilter(options: IOptions){}
+interface Props{
+    filter: number | null;
+    setFilter: React.Dispatch<React.SetStateAction<number | null>>
+}
+
+export default function Filters({filter,setFilter} : Props){
+    function selectFilter(options: IOptions){
+        if(filter === options.id) return setFilter(null);
+        return setFilter(options.id);
+    }
     return (
         <div className={styles.filters}>
             {filters.map((options) => (
-                <button className={styles.filters__filter} key={options.id} onClick={() => selectFilter(options)}>
+                <button className={classNames({
+                    [styles.filters__filter]: true,
+                    [styles['filters__filter--active']]: filter === options.id
+                })} 
+                key={options.id} 
+                onClick={() => selectFilter(options)}
+            >
                     {options.label}
                 </button>
             ))}
