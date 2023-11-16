d75024ff4181b86fad8f565cf1064de8d0658f0a
GabrielMendesCosta <gabrielmendescst@gmail.com>
2023-11-16 11:25:40 -0300

    feat: order has been styled, now almost functional

--- src/pages/Menu/Order/index.tsx
+++ src/pages/Menu/Order/index.tsx
@@ -1,13 +1,27 @@
+import React, { useState } from "react";
 import styles from "./Order.module.scss"
 import options from "./options.json"
+import classNames from "classnames"
+import { MdKeyboardArrowUp, MdKeyboardArrowDown} from "react-icons/md"
 
-export default function Order(){
+interface Props {
+    order: string,
+    setOrder: React.Dispatch<React.SetStateAction<string>>
+}
+
+export default function Order({order, setOrder} : Props){
+    const [open, setOpen] = useState(false);
+    const orderName = order && options.find(options => options.value === order)?.nome;
     return(
-        <button className={styles.order}>
-            <span>Ordenar Por</span>
-            <div className={styles.order__options}>
+        <button className={classNames({[styles.order] : true,[styles["order--active"]] : order !== "",})} onClick={() => setOpen(!open)} onBlur={() => setOpen(false)}>
+            <span>{orderName || "Ordenar Por"}</span>
+            {open ? < MdKeyboardArrowUp size={20} /> : < MdKeyboardArrowDown size={20} />}
+            <div className={classNames({
+                [styles.order__options]: true,
+                [styles["order__options--active"]]: open,
+            })}>
                 {options.map(options => (
-                    <div className={styles.order__option} key={options.value}>
+                    <div className={styles.order__option} key={options.value} onClick={() => setOrder(options.value)}>
                         {options.nome}
                     </div>
                 ))}
